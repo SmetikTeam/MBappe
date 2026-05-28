6c4c2ca346702991d7a4c71c9ee75f4525217097
RinR1n <le6edev2307@yandex.ru>
2026-05-28 19:54:38 +0300

    Add repository

---
+++ MBappe/Repositories/IMotivationProgramRepository.cs
@@ -0,0 +1,17 @@
+﻿using MBappe.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MBappe.Repositories;
+
+public interface IMotivationProgramRepository
+{
+    Task<MotivationProgram?> GetByIdAsync(Guid id);
+
+    Task<IReadOnlyList<MotivationProgram>> GetAllAsync();
+
+    Task AddAsync(MotivationProgram program);
+
+    Task UpdateAsync(MotivationProgram program);
+}
