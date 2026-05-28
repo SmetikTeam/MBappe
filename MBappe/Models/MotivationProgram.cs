ac96aa451241a794a0f1bfcb9170fd5ab0acf9a9
RinR1n <le6edev2307@yandex.ru>
2026-05-28 19:48:09 +0300

    Add models

---
+++ MBappe/Models/MotivationProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MBappe.Models;
+
+public class MotivationProgram
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    public string Title { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public decimal BaseAmount { get; set; }
+
+    public double MinEfficiencyPercent { get; set; } = 60;
+
+    public double MaxEfficiencyPercent { get; set; } = 120;
+
+    public bool IsActive { get; set; } = true;
+
+    public Guid CreatedByUserId { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    public DateTime? UpdatedAt { get; set; }
+}
