a48c6402467bfa750e265bba7afaa48c5638cbe7
RinR1n <le6edev2307@yandex.ru>
2026-05-31 20:19:41 +0300

    Add motivation bonus ef-repository

---
+++ MBappe/Repositories/EfMotivationBonusRepository.cs
@@ -0,0 +1,81 @@
+﻿using MBappe.Data;
+using MBappe.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MBappe.Repositories;
+
+public class EfMotivationBonusRepository : IMotivationBonusRepository
+{
+    public async Task<MotivationBonus?> GetByIdAsync(Guid id)
+    {
+        await using var db = new AppDbContext();
+
+        return await db.MotivationBonuses
+            .AsNoTracking()
+            .FirstOrDefaultAsync(bonus => bonus.Id == id);
+    }
+
+    public async Task<IReadOnlyList<MotivationBonus>> GetAllAsync()
+    {
+        await using var db = new AppDbContext();
+
+        return await db.MotivationBonuses
+            .AsNoTracking()
+            .OrderByDescending(bonus => bonus.CreatedAt)
+            .ToListAsync();
+    }
+
+    public async Task<IReadOnlyList<MotivationBonus>> GetByEmployeeIdAsync(Guid employeeId)
+    {
+        await using var db = new AppDbContext();
+
+        return await db.MotivationBonuses
+            .AsNoTracking()
+            .Where(bonus => bonus.EmployeeId == employeeId)
+            .OrderByDescending(bonus => bonus.CreatedAt)
+            .ToListAsync();
+    }
+
+    public async Task<MotivationBonus?> FindExistingAsync(
+        Guid employeeId,
+        Guid programId,
+        DateTime periodStart,
+        DateTime periodEnd)
+    {
+        await using var db = new AppDbContext();
+
+        var start = periodStart.Date;
+        var end = periodEnd.Date;
+
+        return await db.MotivationBonuses
+            .AsNoTracking()
+            .FirstOrDefaultAsync(bonus =>
+                bonus.EmployeeId == employeeId
+                && bonus.ProgramId == programId
+                && bonus.PeriodStart.Date == start
+                && bonus.PeriodEnd.Date == end
+                && bonus.Status != MotivationBonusStatus.Cancelled);
+    }
+
+    public async Task AddAsync(MotivationBonus bonus)
+    {
+        await using var db = new AppDbContext();
+
+        db.MotivationBonuses.Add(bonus);
+
+        await db.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(MotivationBonus bonus)
+    {
+        await using var db = new AppDbContext();
+
+        db.MotivationBonuses.Update(bonus);
+
+        await db.SaveChangesAsync();
+    }
+}
