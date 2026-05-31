610118ec53f34bf28102c908b2535047246323da
RinR1n <le6edev2307@yandex.ru>
2026-05-31 20:07:10 +0300

    Add user ef-repository

---
+++ MBappe/Repositories/EfEmployeeRepository.cs
@@ -0,0 +1,68 @@
+﻿using MBappe.Data;
+using MBappe.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MBappe.Repositories;
+
+public class EfEmployeeRepository : IEmployeeRepository
+{
+    public async Task<EmployeeProfile?> GetByIdAsync(Guid id)
+    {
+        await using var db = new AppDbContext();
+
+        return await db.Employees
+            .AsNoTracking()
+            .FirstOrDefaultAsync(employee => employee.Id == id);
+    }
+
+    public async Task<EmployeeProfile?> GetByUserIdAsync(Guid userId)
+    {
+        await using var db = new AppDbContext();
+
+        return await db.Employees
+            .AsNoTracking()
+            .FirstOrDefaultAsync(employee => employee.UserId == userId);
+    }
+
+    public async Task<EmployeeProfile?> GetByPersonnelNumberAsync(string personnelNumber)
+    {
+        await using var db = new AppDbContext();
+
+        return await db.Employees
+            .AsNoTracking()
+            .FirstOrDefaultAsync(employee =>
+                employee.PersonnelNumber.ToLower() == personnelNumber.ToLower());
+    }
+
+    public async Task<IReadOnlyList<EmployeeProfile>> GetAllAsync()
+    {
+        await using var db = new AppDbContext();
+
+        return await db.Employees
+            .AsNoTracking()
+            .OrderBy(employee => employee.FullName)
+            .ToListAsync();
+    }
+
+    public async Task AddAsync(EmployeeProfile employee)
+    {
+        await using var db = new AppDbContext();
+
+        db.Employees.Add(employee);
+
+        await db.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(EmployeeProfile employee)
+    {
+        await using var db = new AppDbContext();
+
+        db.Employees.Update(employee);
+
+        await db.SaveChangesAsync();
+    }
+}
