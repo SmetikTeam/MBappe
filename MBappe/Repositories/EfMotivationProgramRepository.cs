2f36e46a111bb76b1fde86a9ca577b6a66eab6d5
RinR1n <le6edev2307@yandex.ru>
2026-05-31 20:18:49 +0300

    Add motivation program ef-repository

---
+++ MBappe/Repositories/EfMotivationProgramRepository.cs
@@ -0,0 +1,50 @@
+﻿using MBappe.Data;
+using MBappe.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MBappe.Repositories;
+
+public class EfMotivationProgramRepository : IMotivationProgramRepository
+{
+    public async Task<MotivationProgram?> GetByIdAsync(Guid id)
+    {
+        await using var db = new AppDbContext();
+
+        return await db.MotivationPrograms
+            .AsNoTracking()
+            .FirstOrDefaultAsync(program => program.Id == id);
+    }
+
+    public async Task<IReadOnlyList<MotivationProgram>> GetAllAsync()
+    {
+        await using var db = new AppDbContext();
+
+        return await db.MotivationPrograms
+            .AsNoTracking()
+            .OrderByDescending(program => program.IsActive)
+            .ThenBy(program => program.Title)
+            .ToListAsync();
+    }
+
+    public async Task AddAsync(MotivationProgram program)
+    {
+        await using var db = new AppDbContext();
+
+        db.MotivationPrograms.Add(program);
+
+        await db.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(MotivationProgram program)
+    {
+        await using var db = new AppDbContext();
+
+        db.MotivationPrograms.Update(program);
+
+        await db.SaveChangesAsync();
+    }
+}
